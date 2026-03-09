c468f3b93a92efcae51654ae3145f68c9489a706
Jelle Bens <jellebens@outlook.com>
2026-03-09 16:45:14 +0100

    Moved ee to its dedicated repo

--- ee/README.md
+++
@@ -1,3 +0,0 @@
-# Troubleshooting
-
-If build of the container fails its possible a galaxy module is missing a dependcy and it needs to be added in bindep.txt
--- ee/build-ee.sh
+++
@@ -1,4 +0,0 @@
-VERSION=$1
-ansible-builder build -v 3 -t jellebens/ansible-excecution-environment:$VERSION --container-runtime podman
-
-podman push jellebens/ansible-excecution-environment:$VERSION
